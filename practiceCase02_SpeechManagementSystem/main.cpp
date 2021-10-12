271014b9eada96de9f158619e44cf7c8e50648c0
gitleej <leej95@163.com>
2021-10-12 09:54:57 +0800

    编写演讲比赛管理系统规则

---
+++ practiceCase02_SpeechManagementSystem/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file    main.cpp
+ * @brief   演讲比赛流程管理系统
+ * @detail  
+ * @section 比赛规则发布
+ * -# 开始比赛前制定比赛规则，规则内容包含参赛选手人数、比赛轮次、每组晋级选手人
+ *    数、比赛分组数量；
+ * -# 每轮晋级选手人数 = 比赛分组数量 X 每组晋级人数；
+ * -# 每轮参赛选手人数 = 比赛分组数量 X 每组选手数量；
+ */
+#include <iostream>
+
+
+int main(int argc, char **argv)
+{
+    system("pause");
+    return 0;
+}
